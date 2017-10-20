76cb94c26d5ceae810ae62043b298d8641194a86
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-10-20 15:39:17 -0200

    M  src/Download.php

--- src/Download.php
+++ src/Download.php
@@ -1,51 +1,76 @@
 <?php
+/**
+* Basic
+* Micro framework em PHP
+*/
 namespace Basic;
-class Download{
-    function get($url,$agent=false,$cookie=false){
+
+/**
+* Classe Download
+*/
+class Download
+{
+    /**
+    * Baixar arquivo através do método GET
+    * @param  string  $url    URL do download
+    * @param  boolean $agent  User agent
+    * @param  boolean $cookie Cookies
+    * @return string          Resposta RAW
+    */
+    public function get(string $url, string $agent=null, array $cookie=null):string
+    {
         $ch = curl_init();
-        if($agent){
+        if ($agent) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: ".$agent));
         }
-        if($cookie){
+        if ($cookie) {
             $cookie_string="";
-            foreach( $cookie as $key => $value ) {
+            foreach ($cookie as $key => $value) {
                 $cookie_string .= "$key=$value;";
             };
-            curl_setopt($ch,CURLOPT_COOKIE, $cookie_string);
+            curl_setopt($ch, CURLOPT_COOKIE, $cookie_string);
         }
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $output=curl_exec($ch);
         curl_close($ch);
         return $output;
     }
-    function post($url,$params=false,$agent=false,$cookie=false){
+    /**
+    * Baixar arquivo através do método POST
+    * @param  string  $url    URL da requisição
+    * @param  boolean $params Parametros da requisição
+    * @param  boolean $agent  User Agent da requisição
+    * @param  boolean $cookie Cookies da requisição
+    * @return string          Resposta RAW
+    */
+    public function post(string $url, array $params=null, array $agent=null, array $cookie=null):string
+    {
         $ch = curl_init();
-        if($agent){
+        if ($agent) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent: ".$agent));
         }
-        if($cookie){
+        if ($cookie) {
             $cookie_string="";
-            foreach( $cookie as $key => $value ) {
+            foreach ($cookie as $key => $value) {
                 $cookie_string .= "$key=$value;";
             };
-            curl_setopt($ch,CURLOPT_COOKIE, $cookie_string);
+            curl_setopt($ch, CURLOPT_COOKIE, $cookie_string);
         }
-        if($params){
+        if ($params) {
             $post_array = '';
-            foreach($params as $k => $v)
-            {
+            foreach ($params as $k => $v) {
                 $post_array .= $k . '='.$v.'&';
             }
             $post_array = rtrim($post_array, '&');
         }
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch,CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, count($post_array));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_array);
-        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,TRUE);
-        curl_setopt($ch,CURLOPT_MAXREDIRS,3);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
         $output=curl_exec($ch);
         curl_close($ch);
         return $output;
